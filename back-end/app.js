8cbcb3632ed5cdd7e4924252389b2d7303845f0f
XaveriusYehuda <xyehuda3@gmail.com>
2025-07-19 21:24:27 +0700

    perbaikan bug angka sensor dashboard

--- back-end/app.js
+++ back-end/app.js
@@ -270,7 +270,7 @@ function connectAwsWebSocket() {
           };
 
           if (simplifiedData.value !== null && !isNaN(simplifiedData.value)) { // Hanya tambahkan jika nilai valid
-              // addDataToCache(nilaiSensor, simplifiedData);
+              addDataToCache(nilaiSensor, simplifiedData);
           }
         });
         // Tampilkan isi cache
